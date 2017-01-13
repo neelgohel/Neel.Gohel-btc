6eca798c05329c52b0b7745f0f2f4f221b3c440a
neelgohel <nealgohel7@yahoo.com>
2017-01-13 16:15:44 +0530

    roboname first commit

---
+++ robot-name/robot_name.rb
@@ -0,0 +1,18 @@
+class Robot
+
+  ALPHA_ARRAY = ('A'.."Z").to_a
+
+
+  def initialize
+    @roboname=''
+    @roboname=ALPHA_ARRAY.shuffle.first + ALPHA_ARRAY.shuffle.first + Time.now.nsec.to_s[-3..-1]
+  end
+
+  def name
+    return @roboname
+  end
+
+  def reset
+    initialize
+  end
+end
