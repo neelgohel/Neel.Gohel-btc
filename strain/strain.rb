94631b0d242ad6d2d2cc8d3459f8dd45bd903a89
neelgohel <nealgohel7@yahoo.com>
2017-01-17 11:04:14 +0530

    Strain first commit

---
+++ strain/strain.rb
@@ -0,0 +1,30 @@
+class Array
+
+  def keep(&block)
+    array_no=[]
+    output=[]
+    array_no=self.map(&block)
+    length_of_array=self.length
+    x=0
+    while length_of_array>0
+      output.push(self[x]) if array_no[x]==true
+      x+=1
+      length_of_array-=1
+    end
+    output
+  end
+
+  def discard(&block)
+    array_no=[]
+    output=[]
+    array_no=self.map(&block)
+    length_of_array=self.length
+    x=0
+    while length_of_array>0
+      output.push(self[x]) if array_no[x]==false
+      x+=1
+      length_of_array-=1
+    end
+    output
+  end
+end
