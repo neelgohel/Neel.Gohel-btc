3731023277305bfca37c816057b6348c76de46ba
Neel Gohel <nealgohel7@yahoo.com>
2017-01-20 16:02:01 +0530

    strain new commit
    changes fixed

--- strain/strain.rb
+++ strain/strain.rb
@@ -1,29 +1,28 @@
 class Array
-
   def keep(&block)
-    array_no=[]
-    output=[]
-    array_no=self.map(&block)
-    length_of_array=self.length
-    x=0
+    array_no = []
+    output = []
+    array_no = self.map(&block)
+    length_of_array = self.length
+    x = 0
     while length_of_array>0
       output.push(self[x]) if array_no[x]==true
-      x+=1
-      length_of_array-=1
+      x += 1
+      length_of_array -= 1
     end
     output
   end
 
   def discard(&block)
-    array_no=[]
-    output=[]
-    array_no=self.map(&block)
-    length_of_array=self.length
-    x=0
+    array_no = []
+    output = []
+    array_no = self.map(&block)
+    length_of_array = self.length
+    x = 0
     while length_of_array>0
       output.push(self[x]) if array_no[x]==false
-      x+=1
-      length_of_array-=1
+      x += 1
+      length_of_array -= 1
     end
     output
   end
