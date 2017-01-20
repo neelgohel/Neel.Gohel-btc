ef3e9e408ea8fa57099b457ab7832042c33eba30
Neel Gohel <nealgohel7@yahoo.com>
2017-01-20 16:14:50 +0530

    ETL changes done

--- etl/etl.rb
+++ etl/etl.rb
@@ -1,52 +1,51 @@
 class ETL
   def self.transform(old)
-    score=[]
-    hash_length=old.length
-    i=0
-    hash_to_array=old.to_a
+    score = []
+    hash_length = old.length
+    i = 0
+    hash_to_array = old.to_a
     while hash_length>0
       score.push(hash_to_array[i][0])
-      i+=1
-      hash_length-=1
+      i += 1
+      hash_length -= 1
     end
-    expected=Hash.new
-    i=0
-    char_array=Array.new()
-    length_of_score=score.length
+    expected = Hash.new
+    i = 0
+    char_array = Array.new()
+    length_of_score = score.length
     while length_of_score>0
       char_array.push(old[score[i]])
-      i+=1
-      length_of_score-=1
+      i += 1
+      length_of_score -= 1
     end
-    length_of_score=score.length
-    str=""
-    i=0
+    length_of_score = score.length
+    str = ""
+    i = 0
     while length_of_score>0
-      str=char_array[i].join("")
-      str=str.downcase
-      char_array[i]=str.split('')
-      i+=1
-      length_of_score-=1
+      str = char_array[i].join("")
+      str = str.downcase
+      char_array[i] = str.split('')
+      i += 1
+      length_of_score -= 1
     end
 
-    length_of_score2=0
-    x=0
-    char=''
-    length_of_score=char_array.length
+    length_of_score2 = 0
+    x = 0
+    char = ''
+    length_of_score = char_array.length
     while length_of_score > 0
-      length_of_score2=char_array[x].length
-      i=0
+      length_of_score2 = char_array[x].length
+      i = 0
         while length_of_score2 >0
-          char=char_array[x][i]
-          i+=1
-          expected[char]=score[x]
-          length_of_score2-=1
+          char = char_array[x][i]
+          i += 1
+          expected[char] = score[x]
+          length_of_score2 -= 1
         end
-      x+=1
-      length_of_score-=1
+      x += 1
+      length_of_score -= 1
     end
     expected = Hash[ expected.sort_by { |key, val| key } ]
-
-    return expected
+    expected
   end
 end
