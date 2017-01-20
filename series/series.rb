851e0886e2a50914b8b35cf922cf0d1449421988
Neel Gohel <nealgohel7@yahoo.com>
2017-01-20 16:05:43 +0530

    Series Changes done

--- series/series.rb
+++ series/series.rb
@@ -1,29 +1,24 @@
 class Series
   def initialize(num)
-    @number=num.to_i
-    @str=num.to_s
-    @length=@str.length
-    puts @number
-    puts @str
-    puts @length
-
+    @number = num.to_i
+    @str = num.to_s
+    @length = @str.length
   end
 
-
   def slices(slice_number)
-    raise ArgumentError if slice_number > @length
+    raise ArgumentError if slice_number>@length
     i=0
-    loop_value=@length-slice_number+1
-    output=[]
+    loop_value = @length - slice_number + 1
+    output = []
     if slice_number==1
-      output=@str.split('')
+      output = @str.split('')
     else
       while loop_value > 0
         output.push(@str[i..i+slice_number-1])
-        i+=1
-        loop_value-=1
+        i += 1
+        loop_value -= 1
       end
     end
-    return output
+    output
   end
 end
