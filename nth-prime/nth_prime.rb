87bee7db5554d665f7805e34ac1cd9fb5d7038d4
Neel Gohel <nealgohel7@yahoo.com>
2017-01-23 11:27:24 +0530

    Update nth_prime.rb

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -2,7 +2,7 @@ require 'prime'
 class Prime
   def  self.nth(num)
     raise ArgumentError  if num == 0
-    prime_numbers=Prime.first 10002
+    prime_numbers = Prime.first 10002
     prime_numbers[num-1]
     end
 end
